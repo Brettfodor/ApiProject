5fea7f38383b5c4e891d71856b6664aa9adbbafd
Brett Fodor <bmattmusic@gmail.com>
2019-10-30 13:53:28 -0400

    backend runs with DBcontext

--- backend/ApiProject/Startup.cs
+++ backend/ApiProject/Startup.cs
@@ -27,14 +27,8 @@ namespace ApiProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<MusicContext>();
-            
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
