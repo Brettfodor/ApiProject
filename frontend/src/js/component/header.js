5b825277e4b1facf96238d83edee6cd11089a734
Brett Fodor <bmattmusic@gmail.com>
2019-10-30 14:12:54 -0400

    front end fixes

--- frontend/src/js/component/header.js
+++ frontend/src/js/component/header.js
@@ -2,9 +2,9 @@ export default function Header() {
     return `
     <nav class='nav__header'> 
     <ul>
-        <li class = 'nav__home'>Artists</li>
-        <li class = 'nav__values'>Songs</li>
-        <li class = 'nav__todos'>Albums</li>
+        <li class = 'nav__artists'>Artists</li>
+        <li class = 'nav__songs'>Songs</li>
+        <li class = 'nav__albums'>Albums</li>
     </ul>    
     <nav/>
 
