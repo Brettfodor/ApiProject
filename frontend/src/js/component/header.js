2d3c35bea12776435ab518c16ea9bc036607944f
Brett Fodor <bmattmusic@gmail.com>
2019-10-30 11:50:47 -0400

    frontend adds

---
+++ frontend/src/js/component/header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return `
+    <nav class='nav__header'> 
+    <ul>
+        <li class = 'nav__home'>Home</li>
+        <li class = 'nav__values'>Value</li>
+        <li class = 'nav__todos'>Todos</li>
+    </ul>    
+    <nav/>
+
+`;
+
+};
