e2c56c646a0100bc13087b7547d7a6ad5f8857ce
Justin Brown <j-brown123@live.com>
2019-11-08 10:39:19 -0500

    stuck on updating song

--- frontend/src/js/component/Song.js
+++ frontend/src/js/component/Song.js
@@ -7,7 +7,12 @@ export default function Song(song){
         <input class="update-song__name" value="${song.title}">
         <button class="update-song__submit">Save Changes</button>
         <input class="update-song__id" type="hidden" value="${song.id}">
-        <input class="update-song__albumId" type="hidden" value="${song.albumId}">
+        <select class= 'update-song__albumId' name="Album">
+        <option value="1">East Atlanta Santa</option>
+        <option value="2">Blink 182</option>
+        <option value="3">The Poison</option>
+        <option value="4">Full Moon Fever</option>
+            </select>
     </section>
     `
 }
